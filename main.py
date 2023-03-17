bd1e4d654d6850624cbb55ccb57d493079acea7b
Amir Giveon <agiveon@gmail.com>
2023-03-17 16:18:52 -0700

    test

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import streamlit as st
 
 
 def show_messages(text):
-    messages_str = [
-        f"{a['role']}: {a['content']}" for a in st.session_state["messages"][1:]
-    ]
-    text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
+    # messages_str = [f"{a['role']}: {a['content']}" for a in st.session_state["messages"][1:]]
+    messages_str = [f"USER: {a['content']}" for a in st.session_state["messages"][1:] if a['role'] == 'user']
+    text.text_area("Messages", value=str("\n".join(messages_str)), height=200)
 
 openai.api_key = st.secrets["openai_key"]
 BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
