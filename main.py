f018fea33fc4f6ae39e2728650df5ec4f27c21f3
Amir Giveon <agiveon@gmail.com>
2023-03-17 15:36:41 -0700

    test

--- main.py
+++ main.py
@@ -1,83 +1,100 @@
-import openai 
+import openai
+import toml
 import streamlit as st
 
-# pip install streamlit-chat  
-from streamlit_chat import message
+
+def show_messages(text):
+    messages_str = [
+        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
+    ]
+    text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
 
 openai.api_key = st.secrets["openai_key"]
+BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
 
-def generate_response(prompt):
-    completions = openai.Completion.create(
-        engine = "text-davinci-003",
-        prompt = prompt,
-        max_tokens = 1024,
-        n = 1,
-        stop = None,
-        temperature=0.5,
-    )
-    message = completions.choices[0].text
-    return message 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = BASE_PROMPT
 
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
+st.header("STREAMLIT GPT-3 CHATBOT")
 
-def show_chat_history():
-    if len(st.session_state['chat_history'])>0:
-        for i in range(len(st.session_state['chat_history'])//2):
-            message(st.session_state['chat_history'][i*2], is_user=True, key=str(i*2) + '_user')
-            message(st.session_state['chat_history'][i*2+1], key=str(i*2+1))
+text = st.empty()
+show_messages(text)
 
-# st.session_state['chat_history'].append('message1')
-# st.session_state['chat_history'].append('message2')
+prompt = st.text_input("Prompt", value="Enter your message here...")
 
+if st.button("Send"):
+    with st.spinner("Generating response..."):
+        st.session_state["messages"] += [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=st.session_state["messages"]
+        )
+        message_response = response["choices"][0]["message"]["content"]
+        st.session_state["messages"] += [
+            {"role": "system", "content": message_response}
+        ]
+        show_messages(text)
 
-show_chat_history()
-
-with st.form("user_input"):
-    # user_input = st.text_input(label='What is your question?')
-
-    user_input = st.text_input(
-        "Enter some text 👇",
-        placeholder='enter text here'
-    )
-
-    user_input_submitted = st.form_submit_button("Submit")
-
-if user_input_submitted:
-    st.session_state['chat_history'].append(user_input)
-    st.session_state['chat_history'].append(user_input * 2)
+if st.button("Clear"):
+    st.session_state["messages"] = BASE_PROMPT
+    show_messages(text)
 
 
-st.subheader('History')
-st.write(st.session_state['chat_history'])
 
-# #Creating the chatbot interface
-# st.title("chatBot : Streamlit + openAI")
 
-# # Storing the chat
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
 
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
 
-# # Display the chat history in reverse order
-# if st.session_state['generated']:
-    
-#     for i in range(len(st.session_state['generated'])-1):
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-#         message(st.session_state["generated"][i], key=str(i))
 
-# # We will get the user's input by calling the get_text function
-# def get_text():
-#     input_text = st.text_input("You: ","Hello, how are you?", key="input")
-#     return input_text
 
-# # Place the text input below the chat history display
-# user_input = get_text()
+# import openai 
+# import streamlit as st
 
-# if user_input:
-#     output = generate_response(user_input)
-#     # store the output 
-#     st.session_state.past.append(user_input)
-#     st.session_state.generated.append(output)
+# # pip install streamlit-chat  
+# from streamlit_chat import message
+
+# openai.api_key = st.secrets["openai_key"]
+
+# def generate_response(prompt):
+#     completions = openai.Completion.create(
+#         engine = "text-davinci-003",
+#         prompt = prompt,
+#         max_tokens = 1024,
+#         n = 1,
+#         stop = None,
+#         temperature=0.5,
+#     )
+#     message = completions.choices[0].text
+#     return message 
+
+# if 'chat_history' not in st.session_state:
+#     st.session_state['chat_history'] = []
+
+# def show_chat_history():
+#     if len(st.session_state['chat_history'])>0:
+#         for i in range(len(st.session_state['chat_history'])//2):
+#             message(st.session_state['chat_history'][i*2], is_user=True, key=str(i*2) + '_user')
+#             message(st.session_state['chat_history'][i*2+1], key=str(i*2+1))
+
+# # st.session_state['chat_history'].append('message1')
+# # st.session_state['chat_history'].append('message2')
+
+
+# show_chat_history()
+
+# with st.form("user_input"):
+#     # user_input = st.text_input(label='What is your question?')
+
+#     user_input = st.text_input(
+#         "Enter some text 👇",
+#         placeholder='enter text here'
+#     )
+
+#     user_input_submitted = st.form_submit_button("Submit")
+
+# if user_input_submitted:
+#     st.session_state['chat_history'].append(user_input)
+#     st.session_state['chat_history'].append(user_input * 2)
+
+
+# st.subheader('History')
+# st.write(st.session_state['chat_history'])
+
