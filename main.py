7f9a6bbf4df326d4ca7d4cce0b1c580aaadb112c
Amir Giveon <agiveon@gmail.com>
2023-03-19 05:36:22 -0700

    test

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ if name_dob_submit:
     st.session_state["horoscope"] = get_horoscope_content(day='today', week = 'current', month = 'current', sign=zodiac_sign)
     st.write(f'✅ Hi {user_name}, got your horoscope - you can proceed to chat.')
     st.write(st.session_state["horoscope"])
-
-BASE_PROMPT = [{"role": "system", "content": f"My name is {user_name}. You are my astrologer named Jane. Answer questions regarding my horoscope: {st.session_state['horoscope']}"}]
+    BASE_PROMPT = [{"role": "system", "content": f"My name is {user_name}. You are my astrologer named Jane. Answer questions regarding my horoscope: {st.session_state['horoscope']}"}]
 
 # BASE_PROMPT = [{"role": "system", "content": "You are my astrologer. Answer my questions about my horoscope"}]
 
