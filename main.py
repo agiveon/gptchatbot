d9112cfdd66a4e44a10e5b8aa52748c1d0d912be
Amir Giveon <agiveon@gmail.com>
2023-03-28 17:25:19 -0700

    test

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ with st.form("myform", clear_on_submit=True):
 
 if submit:
     with st.spinner("Generating response..."):
-        st.session_state["messages"] += [{"role": "user", "content": f'Answer the question: "{prompt}" without apologies, reservations, disclaimers or caveats about astrology. Do not use negatives in your answers. Include references to the locations of the planets and the Sun as much as possible.'}]
+        st.session_state["messages"] += [{"role": "user", "content": f'Answer the question: "{prompt}" without apologies, reservations, disclaimers or caveats about astrology. Do not use negatives in your answers. Include references to the locations of the planets and the Sun from the horoscope.'}]
         
         response = openai.ChatCompletion.create(
             model="gpt-4", 
